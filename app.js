6efbf0e481ed7e005e1e87cfdc3c0ff29f57af80
Gwendolyn Zubatch <gyzubatch@gmail.com>
2018-10-24 17:24:47 -0700

    added question 6; updated question numbering to words (for portability); updated app.js, index.html

--- app.js
+++ app.js
@@ -8,77 +8,125 @@ alert('Welcome, ' + userName + '! I\'m so glad you\'ve stopped by for a visit.  
 var correct = 'That\'s right! (Wait...did you peek at the answer?)';
 var incorrect = 'Awww...too bad. It\'s like you don\'t even know me.';
 
-// Question 1
-console.log('answer1', answer1);
-var answer1 = prompt('Is my middle name \'Yvonne\'?').toUpperCase();
-var correct1 = ' My middle name is indeed \'Yvonne\'. I think my mother was heavily drugged when she named me.';
-console.log('answer1', answer1);
+// set up tally for correct answers
+var totalCorrect = 0;
 
-// Answer evaluation and response for question 1
-if (answer1 === 'Y' || answer1 === 'YES') {
-  alert(correct + correct1);
-} else if (answer1 === 'N' || answer1 === 'NO') {
-  alert(incorrect + correct1);
-} else {
-  alert('Oops! You answered ' + answer1 + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correct1);
+// // Question about middle name
+// console.log('answerMiddlename', answerMiddlename);
+// var answerMiddlename = prompt('Is my middle name \'Yvonne\'?').toUpperCase();
+// var correctMiddlename = ' My middle name is indeed \'Yvonne\'. I think my mother was heavily drugged when she named me.';
+// console.log('answerMiddlename', answerMiddlename);
+
+// // Answer evaluation and response for question about middle name
+// if (answerMiddlename === 'Y' || answerMiddlename === 'YES') {
+//   alert(correct + correctMiddlename);
+//   totalCorrect++;
+// } else if (answerMiddlename === 'N' || answerMiddlename === 'NO') {
+//   alert(incorrect + correctMiddlename);
+// } else {
+//   alert('Oops! You answered ' + answerMiddlename + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correctMiddlename);
+// }
+
+// // Question about birthplace
+// console.log('answerBirthplace', answerBirthplace);
+// var answerBirthplace = prompt('Was I born in Texas?').toUpperCase();
+// var correctBirthplace = ' I was born in Houston. Almost in a taxi, in fact.  How cliche...';
+// console.log('answerBirthplace', answerBirthplace);
+
+// // Answer evaluation and response for question about birthplace
+// if (answerBirthplace === 'Y' || answerBirthplace === 'YES') {
+//   alert(correct + correctBirthplace);
+//   totalCorrect++;
+// } else if (answerBirthplace === 'N' || answerBirthplace === 'NO') {
+//   alert(incorrect + correctBirthplace);
+// } else {
+//   alert('Oops! You answered ' + answerBirthplace + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correctBirthplace);
+// }
+
+// // Question about college
+// console.log('answerCollege', answerCollege);
+// var answerCollege = prompt('Did I go to college?').toUpperCase();
+// var correctCollege = ' I studied Mechanical Engineering at The University of Texas at Austin.  And I ROCKED it.';
+// console.log('answerCollege', answerCollege);
+
+// // Answer evaluation and response for question about college
+// if (answerCollege === 'Y' || answerCollege === 'YES') {
+//   alert(correct + correctCollege);
+//   totalCorrect++;
+// } else if (answerCollege === 'N' || answerCollege === 'NO') {
+//   alert(incorrect + correctCollege);
+// } else {
+//   alert('Oops! You answered ' + answerCollege + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correctCollege);
+// }
+
+// // Question about kids
+// console.log('answerKids', answerKids);
+// var answerKids = prompt('Do I have 2 kids?').toUpperCase();
+// var correctKids = ' I have 3 including my stepchildren! They\'re all grown and gone...sad face.';
+// console.log('answerKids', answerKids);
+
+// // Answer evaluation and response for question about kids
+// if (answerKids === 'N' || answerKids === 'NO') {
+//   alert(correct + correctKids);
+//   totalCorrect++;
+// } else if (answerKids === 'Y' || answerKids === 'YES') {
+//   alert(incorrect + correctKids);
+// } else {
+//   alert('Oops! You answered ' + answerKids + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correctKids);
+// }
+
+// // Question about wedding
+// console.log('answerWedding', answerWedding);
+// var answerWedding = prompt('Will I get married in Hawaii?').toUpperCase();
+// var correctWedding = ' In fact, my dear friends will be married in Hawaii in March, just 2 weeks before I\'m married in FIJI (squeeeee!). I\'ll be on the first flight to Honolulu after I graduate from Code 401!';
+// console.log('answerWedding', answerWedding);
+
+// // Answer evaluation and response for question about wedding
+// if (answerWedding === 'N' || answerWedding === 'NO') {
+//   alert(correct + correctWedding);
+//   totalCorrect++;
+// } else if (answerWedding === 'Y' || answerWedding === 'YES') {
+//   alert(incorrect + correctWedding);
+// } else {
+//   alert('Oops! You answered ' + answerWedding + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correctWedding);
+// }
+
+// Reset user instructions
+alert('Whew... those were tough! But you can handle more. The next questions are a bit tougher because they aren\'t yes or no. I\'ll give you a few chances for each to be fair. Ready?');
+
+// Question about Boeing tenure
+console.log('answerBoeing', answerBoeing);
+var triesLeft = 4; 
+var answerBoeing = parseInt(prompt('How many (whole) years did I work at Boeing? (You have ' + triesLeft + ' tries.)'));
+var correctBoeing = ('I spent 11 years at Boeing, and I learned more than I can recount.');
+console.log('answerBoeing', answerBoeing);
+
+// Answer evaluation and response for question about Boeing tenure
+while(triesLeft > 1) {
+  if (answerBoeing === 11) {
+    alert('That\'s right! ' + correctBoeing);
+    totalCorrect++;
+    break;
+  } else if (answerBoeing < 11 && answerBoeing > 0) {
+    triesLeft--;
+    answerBoeing = parseInt(prompt('Nope... more than that. (You have ' + triesLeft + ' tries left.)'));
+  } else if (answerBoeing > 11 && answerBoeing < 41) {
+    triesLeft--;
+    answerBoeing = parseInt(prompt('Nope... less than that. (You have ' + triesLeft + ' tries left.)'));
+  } else {
+    triesLeft--;
+    answerBoeing = parseInt(prompt('That\'s not even possible! Try again. (You have ' + triesLeft + ' tries left.)'));
+  }
+}
+if (triesLeft === 0) {
+  alert('That was a tough one. ' + correctBoeing);
 }
 
-// Question 2
-console.log('answer2', answer2);
-var answer2 = prompt('Was I born in Texas?').toUpperCase();
-var correct2 = ' I was born in Houston. Almost in a taxi, in fact.  How cliche...';
-console.log('answer2', answer2);
 
-// Answer evaluation and response for question 2
-if (answer2 === 'Y' || answer2 === 'YES') {
-  alert(correct + correct2);
-} else if (answer2 === 'N' || answer2 === 'NO') {
-  alert(incorrect + correct2);
+
+// Report total correct answers
+if (totalCorrect>=4) {
+  alert('Congrats, ' + userName + ', you got ' + totalCorrect + ' answers correct! You seem to know me pretty well. When did we last chat over a coffee? Whenever it was, we\'re certainly overdue... let\'s get together again soon!');
 } else {
-  alert('Oops! You answered ' + answer2 + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correct2);
-}
-
-// Question 3
-console.log('answer3', answer3);
-var answer3 = prompt('Did I go to college?').toUpperCase();
-var correct3 = ' I studied Mechanical Engineering at The University of Texas at Austin.  And I ROCKED it.';
-console.log('answer3', answer3);
-
-// Answer evaluation and response for question 3
-if (answer3 === 'Y' || answer3 === 'YES') {
-  alert(correct + correct3);
-} else if (answer3 === 'N' || answer3 === 'NO') {
-  alert(incorrect + correct3);
-} else {
-  alert('Oops! You answered ' + answer3 + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correct3);
-}
-
-// Question 4
-console.log('answer4', answer4);
-var answer4 = prompt('Do I have 2 kids?').toUpperCase();
-var correct4 = ' I have 3 including my stepchildren! They\'re all grown and gone...sad face.';
-console.log('answer4', answer4);
-
-// Answer evaluation and response for question 4
-if (answer4 === 'N' || answer4 === 'NO') {
-  alert(correct + correct4);
-} else if (answer4 === 'Y' || answer4 === 'YES') {
-  alert(incorrect + correct4);
-} else {
-  alert('Oops! You answered ' + answer4 + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correct4);
-}
-
-// Question 5
-console.log('answer5', answer5);
-var answer5 = prompt('Will I get married in Hawaii?').toUpperCase();
-var correct5 = ' In fact, my dear friends will be married in Hawaii in March, just 2 weeks before I\'m married in FIJI (squeeeee!). I\'ll be on the first flight to Honolulu after I graduate from Code 401!';
-console.log('answer5', answer5);
-
-// Answer evaluation and response for question 5
-if (answer5 === 'N' || answer5 === 'NO') {
-  alert(correct + correct4);
-} else if (answer5 === 'Y' || answer5 === 'YES') {
-  alert(incorrect + correct5);
-} else {
-  alert('Oops! You answered ' + answer5 + ', which isn\'t y/yes/n/no. Didn\'t we talk about how to do this? ' + correct5);
+  alert('Gosh, ' + userName + ', you only got ' + totalCorrect + ' answers correct. We should probably get to know one another better over a cup of coffee. Reach out - let\'s chat!');
 }
